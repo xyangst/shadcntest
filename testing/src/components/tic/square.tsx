886d95b2b28c4f206533ac94cdc5e4685fa32081
DerShredder <sinxtra69@gmail.com>
2023-06-24 22:18:26 +0200

    made text bigger

--- testing/src/components/tic/square.tsx
+++ testing/src/components/tic/square.tsx
@@ -5,7 +5,7 @@ export default function Square({ Value, onSquareClick }) {
   return (
     <button
       onClick={onSquareClick}
-      className="font-bold w-10 h-10 md:w-12 md:h-12 xl:md:h-14 xl:md:w-14 border border-white  p-0"
+      className="font-bold w-10 h-10 md:w-12 md:h-12 xl:md:h-14 xl:md:w-14 border text-3xl  p-0"
     >
       {Value}
     </button>
