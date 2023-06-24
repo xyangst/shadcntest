566951a29d1adf47e96073424594caf8cdc83807
DerShredder <sinxtra69@gmail.com>
2023-06-24 03:53:07 +0200

    small design change

--- testing/src/components/main/NavBar.tsx
+++ testing/src/components/main/NavBar.tsx
@@ -2,13 +2,13 @@ import { FaGithub, FaHome } from "react-icons/fa";
 import Link from "next/link";
 export default function CommandMenu() {
   return (
-    <nav className="fixed top-2 left-2">
-      <Link className=" m-2 rounded-md text-3xl " href="/">
+    <nav className="fixed top-1 left-1 border">
+      <Link className=" m-0 rounded-md text-3xl " href="/">
         <FaHome />
       </Link>
       <Link
         target="_blank"
-        className="m-2 rounded-md text-3xl "
+        className="m-0 rounded-md text-3xl "
         href="https://github.com/xyangst/shadcntester"
       >
         <FaGithub />
